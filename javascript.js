2450c60b9da5670eeea19103875f9aa49c592a70
bahriddin <bahriddin@gmail.com>
2022-03-24 16:01:35 +0300

    .

---
+++ javascript.js
@@ -0,0 +1,34 @@
+let str = prompt("istalgan matinni kiriting", "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis, sint!")
+let o = "o";
+let a = "a";
+let i = "i";
+let e = "e";
+let u = "u";
+let probel = " ";
+let vergul = ",";
+let nuqta = ".";
+let undov = "!";
+let total = 0;
+let total1 = 0;
+let total2 = 0;
+let arr = str.split("")
+for (let text = 0; text < arr.length; text++) {
+    const element = arr[text];
+    
+   let el = (element == a || element == o || element == i || element == e|| element == u);
+   
+   let il = (element == undov ||element == probel || element == vergul || element == nuqta);
+   
+if (el == true) {
+     total += 1;
+}
+else if (il == true) {
+    total1 += 1;
+}
+else{
+    total2 += 1; 
+}
+} console.log(" unli hariflar soni: " +total); 
+console.log(" simvolar va probellar soni: " +total1); 
+ console.log(" undosh hariflar soni: " +total2);
+ console.log(" umumiy belgi, probel va hariflar soni: " +arr.length);
