cf492d350908f5c0d5eb632d384a64e282d2e0f1
bahriddin <bahriddin@gmail.com>
2022-03-24 16:09:49 +0300

    .

--- javascript.js
+++ javascript.js
@@ -4,6 +4,13 @@ let a = "a";
 let i = "i";
 let e = "e";
 let u = "u";
+
+let O = "O";
+let A = "A";
+let I = "I";
+let E = "E";
+let U = "U";
+
 let probel = " ";
 let vergul = ",";
 let nuqta = ".";
@@ -15,7 +22,7 @@ let arr = str.split("")
 for (let text = 0; text < arr.length; text++) {
     const element = arr[text];
     
-   let el = (element == a || element == o || element == i || element == e|| element == u);
+   let el = (element == a || element == o || element == i || element == e|| element == u || element == A || element == O || element == I || element == E|| element == U);
    
    let il = (element == undov ||element == probel || element == vergul || element == nuqta);
    
